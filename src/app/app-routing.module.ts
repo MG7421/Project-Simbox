ff9eb4261a99efdca30162d1659644676e5e68e6
DevOps <devops@MacBook-Pro-de-DevOps.local>
2024-02-23 11:33:59 +0000

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthGuard } from './core/guards/auth.guard';
+import { LayoutComponent } from './layouts/layout.component';
+import { CyptolandingComponent } from './cyptolanding/cyptolanding.component';
+import { Page404Component } from './extrapages/page404/page404.component';
+import { Login2Component } from './account/auth/login2/login2.component';
+
+const routes: Routes = [
+  { path: 'login2', component: Login2Component },
+  { path: '', redirectTo: '/login2', pathMatch: 'full' },
+  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
+  // tslint:disable-next-line: max-line-length
+  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard] },
+  { path: 'pages', loadChildren: () => import('./extrapages/extrapages.module').then(m => m.ExtrapagesModule), canActivate: [AuthGuard] },
+  { path: 'crypto-ico-landing', component: CyptolandingComponent },
+  { path: '**', component: Page404Component },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
